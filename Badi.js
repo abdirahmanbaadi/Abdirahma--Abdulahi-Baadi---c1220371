8058b42dfad147f7ccf17625860652ae81519b99
abdirahmanbaadi <167464701+abdirahmanbaadi@users.noreply.github.com>
2025-11-09 11:31:32 -0800

    Add files via upload

---
+++ Badi.js
@@ -0,0 +1,54 @@
+const { MongoClient } = require('mongodb');
+
+const client = new MongoClient('mongodb://localhost:27017');
+
+async function run() {
+  try {
+    await client.connect();
+    console.log('Connected to MongoDB');
+
+    const db = client.db('university');
+    const studen = db.collection('studen');
+       const insertResult = await studen.insertMany([
+      { name: "Maryan", age: 21, department: "Accounting", year: 2 },
+      { name: "Samar", age: 20, department: "BCS", year: 2 },
+      { name: "Sihaam", age: 26, department: "Engineering", year: 4 }
+    ]);
+
+    const allstudents = await studen.find().toArray();
+    console.log("All students:", allstudents);
+
+    const updateResult = await studen.updateOne(
+      { name: "Sihaam" },       
+      { $set: { year: 5 } }       
+    );
+
+    console.log('Matched Documents:', updateResult.matchedCount);
+    console.log('Modified Documents:', updateResult.modifiedCount);
+
+  
+const deleteManyResult = await studen.deleteMany({ name: "Samar"});
+console.log("Deleted Many:", deleteManyResult.deletedCount);
+
+
+const deleteOneResult = await studen.deleteOne({ name: "jihaan"});
+console.log("Deleted One:", deleteOneResult.deletedCount);
+
+
+     const updateresult = await studen.updateMany(
+      { name: "Sihaam" },       
+      { $set: { year: 5 } }       
+    );
+
+
+
+        
+  } catch (err) {
+    console.error('Error:', err);
+  } finally {
+    await client.close();
+    console.log('Connection closed');
+  }
+}
+
+run();
